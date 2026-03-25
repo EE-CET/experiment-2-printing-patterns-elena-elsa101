b86f1f8996875039bb58cf03f6f0585087a3e0b0
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-25 20:22:00 +0530

    Update PatternPrinting.java

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // Horizontal line
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+
+        // Move to next line
+        System.out.println();
+
+        // Vertical line
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+        
+
 }
